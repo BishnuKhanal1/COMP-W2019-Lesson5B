4ef2f502d8b836e2ef309fc8522f9fff8e76af8c
Mr. Bishnu Prasad Khanal <bkhanal1@my.centennialcollege.ca>
2019-02-09 14:52:10 -0500

    added create  functionality to the database

--- routes/contact.js
+++ routes/contact.js
@@ -6,12 +6,12 @@ let mongoose = require("mongoose");
 //create a reference to the database schema (which is in in models)
 //for this we need models folder //make models folder
 
-let contact = require("../models/contact"); //reference to contact.js file in models folder
+let contactModel = require("../models/contact"); //reference to contact.js file in models folder
 
 //now we need to use the router
 /*Get Contact List Page - read operation */
 router.get("/", (req, res, next) => {
-  contact.find((err, contactList) => {
+  contactModel.find((err, contactList) => {
     if (err) {
       return console.error(err);
     } else {
@@ -24,5 +24,28 @@ router.get("/", (req, res, next) => {
     }
   });
 });
+//get route for the add page/ this willl display the add page
+router.get("/add", (req, res, next) => {
+  res.render("contacts/add", {
+    title: "Add New Contact"
+  });
+});
+//post route
+router.post("/add", (req, res, next) => {
+  //console.log(req.body);
 
+  let newContact = contactModel({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName
+  });
+  contactModel.create(newContact, (err, contactModel) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    } else {
+      //refersh contact list
+      res.redirect("contact-list");
+    }
+  });
+});
 module.exports = router;
